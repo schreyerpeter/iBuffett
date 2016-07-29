9df02b4fc16289c67832806099ff0e2e9ee2b6ac
schreyerpeter <pschreyer12321@aol.com>
2016-07-29 16:26:59 -0700

    Stock price history graph now displays upon search. Need to remove previous graph upon generation of new one.#2

--- public/stocks/stocks.controller.js
+++ public/stocks/stocks.controller.js
@@ -18,6 +18,62 @@ function stocks($http, $interval){
         vm.changePercent = datum.data.query.results.quote.PercentChange;
         vm.change = datum.data.query.results.quote.Change;
         vm.previous = datum.data.query.results.quote.PreviousClose;
+
+        var margin = {top: 20, right: 20, bottom: 30, left: 50},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+        // parse the date / time
+        var parseTime = d3.timeParse("%Y-%m-%d");
+        // set the ranges
+        var x = d3.scaleTime().range([0, width]);
+        var y = d3.scaleLinear().range([height, 0]);
+        // define the line
+        var valueline = d3.line()
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y(d.close); });
+        // append the svg obgect to the body of the page
+        // appends a 'group' element to 'svg'
+        // moves the 'group' element to the top left margin
+        var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+        // Get the data
+        d3.request('https://www.quandl.com/api/v3/datasets/WIKI/'+vm.symbol+'.json?api_key=Cf5fdwzihyoxY8HwbBHT', function(error, data) {
+          if (error) throw error;
+          // format the data
+          var dataPoints = [];
+          var jData = (JSON.parse(data.response));
+          console.log(jData);
+          jData.dataset.data.forEach(function(arr){
+            var dataPoint = {};
+            dataPoint.date = arr[0];
+            dataPoint.close = arr[4];
+            dataPoints.push(dataPoint);
+          })
+          console.log(dataPoints);
+          dataPoints.forEach(function(d) {
+            d.date = parseTime(d.date);
+            d.close = +d.close;
+          });
+          // Scale the range of the data
+          x.domain(d3.extent(dataPoints, function(d) { return d.date; }));
+          y.domain([0, d3.max(dataPoints, function(d) { return d.close; })]);
+          // Add the valueline path.
+          svg.append("path")
+          .data([dataPoints])
+          .attr("class", "line")
+          .attr("d", valueline);
+          // Add the X Axis
+          svg.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));
+          // Add the Y Axis
+          svg.append("g")
+          .call(d3.axisLeft(y));
+        });
       }
       else {
         vm.name = "Invalid ticker symbol";
