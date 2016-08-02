5fd0bdbdf2961f3729a11dc8f7a658a24bd714de
schreyerpeter <pschreyer12321@aol.com>
2016-08-02 12:39:10 -0700

    Improved functionality and aesthetics of the search function.#2

--- public/default.js
+++ public/default.js
@@ -1,5 +1,14 @@
+function toggle(showPage, hidePage){
+  hidePage.className = hidePage.className + ' hidden';
+  showPage.className = showPage.className.replace(/hidden/g,' ');
+}
+
 var stockBox = document.getElementById('stock-box');
-var search = document.getElementById('search');
+
 search.addEventListener('click', function(e){
   stockBox.className = '';
+})
+var start = document.getElementById('start');
+start.addEventListener('click', function(e){
+  toggle(container,intro);
 })
