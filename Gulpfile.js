0dbcf41663e1e1281fb9764c0ae9df4228c8e1e9
schreyerpeter <pschreyer12321@aol.com>
2016-09-14 12:56:43 -0700

    Gulpfile bug fixed.#9

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,13 +2,12 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var gutil = require('gulp-util');
 
-gulp.task('default', function() {
-  gulp.watch(['public/*.js','/*.js'], ['mocha']);
+gulp.task('watch', function() {
+  gulp.watch(['public/*.js','*.js'], ['mocha']);
 });
 
 gulp.task('mocha', function(event) {
   return gulp.src(['public/app.spec.js'], { read: false})
-    .pipe(mocha({ reporter : "list" }))
+    .pipe(mocha({ reporter : 'spec' }))
     .on('error', gutil.log);
-  console.log(event.path, event.type);
 });
