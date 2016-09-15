f7006c3165e8fb14f86f6c03800033a825e1d525
schreyerpeter <pschreyer12321@aol.com>
2016-09-15 05:10:33 -0700

    App now works on Heroku.#10

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var unirest = require('unirest');
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
